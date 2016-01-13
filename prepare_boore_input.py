dd1954679181ab06a07b5eb5ff64b9f2fd3d01e8
James Holt <jamesholt@jamesH.local>
2016-01-13 11:39:29 +0000

    Writes data to file correctly, need to add function to write eqparams.

--- prepare_boore_input.py
+++ prepare_boore_input.py
@@ -23,25 +23,20 @@ import os.path
 #... argv[2] is dip in degrees, argv[3] is the magnitude (Mw) and argv[4] 
 #... is the output file name.
 
-with open('filenames.txt', 'w') as fnames:   
-    for f in filenames1:
-        fnames.write(f + '\n')
-    for f in filenames2:
-        fnames.write(f + '\n')
-    for f in filenames3:
-        fnames.write(f + '\n')
-
 def main():
-    path_to_folder = argv[1]
+    path_to_folder = '/Users/jamesholt/Documents/MRes/20001006133000/20001006133000.kik/'
     
     
-    f1, f2, f3, f4, f5, f6 = grab_filenames(path_to_folder)
+    fileList= grab_file_names(path_to_folder)
+    for fname in fileList:
+        make_db_files(fname)
         
-    write_fname_to_file(f1, f2, f3, f4, f5, f6)
+    write_fname_to_file(fileList)
     
-    with open ('filenames.txt', 'r') as f:
-        for fname in f:
-            make_db_files(fname)
+    
+    
+        
+      
 
 
 def grab_file_names(path_to_folder):
@@ -49,49 +44,50 @@ def grab_file_names(path_to_folder):
         to the folder. Glob then searches for the file extension below and 
         stores the filenames in a list. The function retuns a list for each
         of the glob searches separately. """ 
-    filenames1 = glob(path_to_folder + '*.EW1.h')
+    f1 = glob(path_to_folder + '*.EW1.h')
 
-    filenames2 = glob(path_to_folder + '*.NS1.h')
+    f2 = glob(path_to_folder + '*.NS1.h')
 
-    filenames3 = glob(path_to_folder + '*.UD1.h')
+    f3 = glob(path_to_folder + '*.UD1.h')
 
-    filenames4 = glob(path_to_folder + '*.EW2.h')
+    f4 = glob(path_to_folder + '*.EW2.h')
     
-    filenames5 = glob(path_to_folder + '*.NS2.h')
+    f5 = glob(path_to_folder + '*.NS2.h')
 
-    filenames6 = glob(path_to_folder + '*.UD2.h')
+    f6 = glob(path_to_folder + '*.UD2.h')
 
-    return filenames1, filenames2, filenames3, filenames4, filenames5, filenames6
+    joinedFileList =  f1 + f2 + f3 + f4 + f5 + f6
+
+    return joinedFileList
 
     
 
-def write_fname_to_file(fname1, fname2, fname3, fname4, fname5, fname6):
+def write_fname_to_file(fname):
     """ This function writes all of the file paths and names to a single file. """
     with open('filenames.txt', 'w') as fnames:   
-        for f in fname1:
+        for f in fname:
             fnames.write(f + '\n')
-        for f in fname2:
-            fnames.write(f + '\n')
-        for f in fname3:
-            fnames.write(f + '\n')
-        for f in fname4:
-            fnames.write(f + '\n')
-        for f in fname5:
-            fnames.write(f + '\n')
-        for f in fname6:
-            fnames.write(f + '\n')
+       
 
 def make_db_files(fname):
-    dip = float(argv[2])
-    mag = float(argv[3])
+    dip = float(90.0)
+    mag = float(7.3)
  
-    with open (argv[1] + argv[4], 'ab') as f:          
+    with open ('testes.txt', 'ab') as f:          
             
         h = np.loadtxt(fname)
         EpiD, HypD = hyper_epiD(h)
         
         dbfilearray = np.array([h[10], h[11], h[12], h[23]/100, EpiD/1000, HypD/1000])
         np.savetxt(f, dbfilearray)
+    
+    reshapeFile()
+
+    
+def reshapeFile():
+    
+    dat = np.loadtxt('testes.txt')
+    np.savetxt('testes.txt', dat.reshape(len(dat) / 6, 6))
 
 #def check_if_exist(path_to_file, filename):
     #if os.path.isfile(path_to_file + filename) == False:
